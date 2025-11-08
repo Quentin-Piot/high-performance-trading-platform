c817b4e4dd80fece0e9854b8729e658afb80f374
quentinpiot <qkpiot@gmail.com>
2025-11-08 10:16:14 -0300

    refactor(terraform): refactor monte carlo components and optimize resources
    - Remove SQS queues and related IAM policies for Monte Carlo jobs
    - Remove Monte Carlo worker log group and Redis container
    - Reduce CloudWatch log retention from 14/30 to 7 days
    - Optimize ECS task CPU/memory allocation and use alpine postgres

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -4,7 +4,7 @@
 
 resource "aws_cloudwatch_log_group" "ecs" {
   name              = "/ecs/${var.project_name}"
-  retention_in_days = 14
+  retention_in_days = 7  # Réduit de 14 à 7 jours pour économiser
 
   tags = {
     Name = "${var.project_name}-ecs-logs"
@@ -13,18 +13,9 @@ resource "aws_cloudwatch_log_group" "ecs" {
 
 resource "aws_cloudwatch_log_group" "application" {
   name              = "/aws/application/${var.project_name}"
-  retention_in_days = 30
+  retention_in_days = 7
 
   tags = {
     Name = "${var.project_name}-app-logs"
-  }
-}
-
-resource "aws_cloudwatch_log_group" "monte_carlo_worker" {
-  name              = "/aws/worker/${var.project_name}-monte-carlo"
-  retention_in_days = 30
-
-  tags = {
-    Name = "${var.project_name}-worker-logs"
   }
 }
